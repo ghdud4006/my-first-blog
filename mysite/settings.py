e06ba6ca6d711754b7b98493ca46e135af854e35
ghdud4006 <ghdud4006@gmail.com>
2018-01-08 21:58:32 +0900

    Modify Allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '82h9l+(us&%vl&o95^$0x5=-(sbb$mq^6$m@1)d5@fx7g1-wfx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
